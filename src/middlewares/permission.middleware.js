07c45c041ed9a5c4fa5622ad337e42ef22a3d82e
David Chamling Rai <Davidrai441@gmail.com>
2023-12-28 17:27:16 +0545

    check any of role and permission middleware added

--- src/middlewares/permission.middleware.js
+++ src/middlewares/permission.middleware.js
@@ -24,3 +24,26 @@ export function checkPermissions(...permissions) {
     }
   })
 }
+export function checkAnyPermissions(...permissions) {
+  return asyncHandler((req, res, next) => {
+    // fetching authenticated user
+    const user = req.user
+
+    // validating user
+    if (user && user.permissions) {
+      // fetching user's permissions
+      const userPermissions = user.permissions.map((perm) => perm.name)
+
+      // validating permissions
+      if (
+        permissions.some((permission) => userPermissions.includes(permission))
+      ) {
+        next()
+      } else {
+        throw new ApiError(403, 'Permission denied')
+      }
+    } else {
+      throw new ApiError(401, 'Unauthorized')
+    }
+  })
+}
--- src/middlewares/role.middleware.js
+++ src/middlewares/role.middleware.js
@@ -23,3 +23,25 @@ export function checkRoles(...roles) {
     }
   })
 }
+export function checkAnyRoles(...roles) {
+  return asyncHandler((req, res, next) => {
+    // fetching authenticated user
+    const user = req.user
+
+    console.log(user, roles)
+    // validating user
+    if (user && user.roles) {
+      // fetching user's roles
+      const userRoles = user.roles.map((role) => role.name)
+
+      // validating roles
+      if (roles.some((role) => userRoles.includes(role))) {
+        next()
+      } else {
+        throw new ApiError(403, 'Role-based permission denied')
+      }
+    } else {
+      throw new ApiError(401, 'Unauthorized')
+    }
+  })
+}
